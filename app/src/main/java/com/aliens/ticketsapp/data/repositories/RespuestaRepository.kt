f01a6ff7e0149cb82485d29c01ce02d80bf51ce0
PrandiFabiel <fabiel098@gmail.com>
2022-07-11 17:05:27 -0400

    Se creo el respository de Respuesta.

---
+++ app/src/main/java/com/aliens/ticketsapp/data/repositories/RespuestaRepository.kt
@@ -0,0 +1,26 @@
+package com.aliens.ticketsapp.data.repositories
+
+import com.aliens.ticketsapp.data.daos.RespuestaDao
+import com.aliens.ticketsapp.model.Respuesta
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class RespuestaRepository @Inject constructor(
+    val respuestaDao: RespuestaDao
+){
+    suspend fun insertar(respuesta: Respuesta){
+        respuestaDao.insertar(respuesta)
+    }
+
+    suspend fun eliminar(respuesta: Respuesta){
+        respuestaDao.eliminar(respuesta)
+    }
+
+    fun buscar(respuestaId: Int): Flow<List<Respuesta>> {
+        return respuestaDao.buscar(respuestaId)
+    }
+
+    fun getList(): Flow<List<Respuesta>>{
+        return respuestaDao.getList()
+    }
+}
