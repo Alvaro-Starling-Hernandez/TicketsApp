3d544281838be0f00b8704d216cf001f0e20da86
Alvaro S. Hernandez Luzon <alvarostarling24@gmail.com>
2022-07-11 15:40:32 -0400

    Crando TiempoDao.kt

---
+++ app/src/main/java/com/aliens/ticketsapp/data/TiempoDao.kt
@@ -0,0 +1,20 @@
+package com.aliens.ticketsapp.data
+
+import androidx.room.*
+import com.aliens.ticketsapp.model.Tiempo
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface TiempoDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertar(tiempo: Tiempo)
+
+    @Delete
+    suspend fun eliminar(tiempo: Tiempo)
+
+    @Query("SELECT * FROM tiempos WHERE tiempoId =:tiempoId")
+    fun buscar(tiempoId: Int): Flow<List<Tiempo>>
+
+    @Query("SELECT * FROM tiempos ORDER BY tiempoId")
+    fun getList(): Flow<List<Tiempo>>
+}
