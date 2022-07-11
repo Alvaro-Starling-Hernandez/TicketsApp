13599ab9218d35c9a26122e33f0a571b5bb8c2a5
Alvaro S. Hernandez Luzon <alvarostarling24@gmail.com>
2022-07-11 15:45:15 -0400

    Crando primera version de la base de datos

---
+++ app/src/main/java/com/aliens/ticketsapp/data/TicketsDb.kt
@@ -0,0 +1,14 @@
+package com.aliens.ticketsapp.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.aliens.ticketsapp.model.Tiempo
+
+@Database(
+    entities = [Tiempo::class],
+    exportSchema = false,
+    version = 1
+)
+abstract class TicketsDb: RoomDatabase() {
+    abstract val tiempoDao: TiempoDao
+}
