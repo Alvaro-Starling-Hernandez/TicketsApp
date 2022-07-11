ed0f61bbaa18d56c59894649f90c1eee4e2a3e31
Alvaro S. Hernandez Luzon <alvarostarling24@gmail.com>
2022-07-11 15:49:03 -0400

    Crando TiempoRepository.kt

---
+++ app/src/main/java/com/aliens/ticketsapp/data/TiempoRepository.kt
@@ -0,0 +1,25 @@
+package com.aliens.ticketsapp.data
+
+import com.aliens.ticketsapp.model.Tiempo
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class TiempoRepository @Inject constructor(
+    val tiempoDao: TiempoDao
+){
+    suspend fun insertar(tiempo: Tiempo){
+        tiempoDao.insertar(tiempo)
+    }
+
+    suspend fun eliminar(tiempo: Tiempo){
+        tiempoDao.eliminar(tiempo)
+    }
+
+    fun buscar(tiempId: Int): Flow<List<Tiempo>> {
+        return tiempoDao.buscar(tiempId)
+    }
+
+    fun getList(): Flow<List<Tiempo>>{
+        return tiempoDao.getList()
+    }
+}
