4d469cecebf338491b661ecc365bed534ec9bb04
Alvaro S. Hernandez Luzon <alvarostarling24@gmail.com>
2022-07-11 17:29:19 -0400

    Agregando TiempoViewModel.kt

---
+++ app/src/main/java/com/aliens/ticketsapp/ui/screens/tiempo/TiempoViewModel.kt
@@ -0,0 +1,36 @@
+package com.aliens.ticketsapp.ui.screens.tiempo
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.aliens.ticketsapp.data.repositories.TiempoRepository
+import com.aliens.ticketsapp.model.Tiempo
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class TiempoViewModel @Inject constructor(
+    val tiempoRepository: TiempoRepository
+): ViewModel() {
+    var trabajo by mutableStateOf("")
+    var tiempo by mutableStateOf("")
+
+    var tiempos = tiempoRepository.getList()
+        private set
+
+    fun Guardar() {
+        viewModelScope.launch {
+            tiempoRepository.insertar(
+                Tiempo(
+                    trabajo = trabajo,
+                    tiempo = tiempo.toFloat(),
+                    tecnicoId = 0,
+                    ticketId = 0
+                )
+            )
+        }
+    }
+}
