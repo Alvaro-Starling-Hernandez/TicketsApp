84aeace7d267a1dac03b59ca55d2b31cb3031329
Alvaro S. Hernandez Luzon <alvarostarling24@gmail.com>
2022-07-12 12:59:27 -0400

    Cambiando la version de la navegacion y modificando el spinner de tecnicos

--- app/src/main/java/com/aliens/ticketsapp/ui/components/TecnicoSpinner.kt
+++ app/src/main/java/com/aliens/ticketsapp/ui/components/TecnicoSpinner.kt
@@ -4,35 +4,31 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.DropdownMenu
-import androidx.compose.material.Icon
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material.icons.filled.Person
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.toSize
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.aliens.ticketsapp.R
+import com.aliens.ticketsapp.ui.screens.tecnico.TecnicoViewModel
+import com.aliens.ticketsapp.ui.screens.tiempo.TiempoViewModel
 
 @Composable
 fun TecnicoSpinner(
-    //viewModel
+    viewModel: TecnicoViewModel = hiltViewModel()
 ) {
 
     var mExpanded by remember { mutableStateOf(false) }
 
-    //val ocupaciones = viewModel.ocupaciones.collectAsState(initial = emptyList())
+    val tecnicos = viewModel.tecnicos.collectAsState(initial = emptyList())
 
     var mSelectedText by remember {
         mutableStateOf("")
@@ -78,15 +74,15 @@ fun TecnicoSpinner(
             onDismissRequest = { mExpanded = false },
             modifier = Modifier.width(with(LocalDensity.current) { mTextFieldSize.width.toDp() })
         ) {
-//            ocupaciones.value.forEach {
-//                DropdownMenuItem(onClick = {
-//                    personaViewModel.ocupacion = it.ocupacionId
-//                    mSelectedText = it.nombre
-//                    mExpanded = false
-//                }) {
-//                    Text(text = it.nombre)
-//                }
-//            }
+            tecnicos.value.forEach {
+                DropdownMenuItem(onClick = {
+                    //personaViewModel.ocupacion = it.ocupacionId
+                    mSelectedText = it.tecnicoId.toString()
+                    mExpanded = false
+                }) {
+                    Text(text = it.tecnicoId.toString())
+                }
+            }
         }
     }
 }
