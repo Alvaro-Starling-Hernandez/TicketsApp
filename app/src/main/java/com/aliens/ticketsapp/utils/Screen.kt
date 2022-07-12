ba7f224bf5062a31ff8b6aa6a09d44d7328008db
PrandiFabiel <fabiel098@gmail.com>
2022-07-12 10:27:13 -0400

    Creando clase screen con las rutas para la navegacion.

---
+++ app/src/main/java/com/aliens/ticketsapp/utils/Screen.kt
@@ -0,0 +1,28 @@
+package com.aliens.ticketsapp.utils
+
+sealed class Screen(val route: String){
+    object RegistroRespuesta: Screen("RegistroRespuesta")
+    object ConsultaRespuesta: Screen("ConsultaRespuesta")
+    object RegistroTiempo: Screen("RegistroTiempo")
+    object ConsultaTiempo: Screen("ConsultaTiempo")
+    object RegistroTecnico: Screen("RegistroTecnico")
+    object ConsultaTecnico: Screen("ConsultaTecnico")
+
+    fun withArgs(vararg args: String): String {
+        return buildString {
+            append(route)
+            args.forEach{ arg ->
+                append("/$arg")
+            }
+        }
+    }
+
+    fun withArgsFormat(vararg args: String) : String {
+        return buildString {
+            append(route)
+            args.forEach{ arg ->
+                append("/{$arg}")
+            }
+        }
+    }
+}
