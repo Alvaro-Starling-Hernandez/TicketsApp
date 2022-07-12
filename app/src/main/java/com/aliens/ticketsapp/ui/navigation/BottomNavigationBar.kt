231898ee9a544aeda936bbb123eefde9d0815316
PrandiFabiel <fabiel098@gmail.com>
2022-07-12 11:17:13 -0400

    Configurando la navegacion inferior(BottomNav)

---
+++ app/src/main/java/com/aliens/ticketsapp/ui/navigation/BottomNavigationBar.kt
@@ -0,0 +1,76 @@
+package com.aliens.ticketsapp.ui.navigation
+
+import androidx.annotation.StringRes
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.aliens.ticketsapp.R
+import com.aliens.ticketsapp.utils.Screen
+
+sealed class BottomNavItem(
+    val route: String,
+    @StringRes val titleResId: Int,
+    val icon: ImageVector
+) {
+    object Home : BottomNavItem(
+        route = Screen.ConsultaRespuesta.route,
+        titleResId = R.string.Respuestas,
+        icon = Icons.Default.Home
+    )
+
+    object Tecnico : BottomNavItem(
+        route = Screen.RegistroTecnico.route,
+        titleResId = R.string.Tecnico,
+        icon = Icons.Default.Settings
+    )
+}
+
+@Composable
+fun BottomNavigationBar(
+    navController: NavController
+) {
+    val items = listOf(
+        BottomNavItem.Home,
+        BottomNavItem.Tecnico
+    )
+
+    BottomNavigation {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+        items.forEach { item ->
+            BottomNavigationItem(
+                icon = {
+                    Icon(
+                        imageVector = item.icon,
+                        contentDescription = stringResource(id = item.titleResId)
+                    )
+                },
+                label = { Text(text = stringResource(id = item.titleResId)) },
+                selected = currentRoute == item.route,
+                onClick = {
+                    navController.navigate(item.route) {
+
+                        navController.graph.startDestinationRoute?.let { route ->
+                            popUpTo(route) {
+                                saveState = true
+                            }
+                        }
+
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                }
+            )
+        }
+    }
+}
