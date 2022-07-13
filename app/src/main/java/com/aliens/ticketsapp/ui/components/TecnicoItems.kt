711ad6a508eaaa0eefa35106d6b2dcaac1830a93
Frankelyn <Frankelyn_garcia@ucne.edu.do>
2022-07-13 17:54:01 -0400

    Modificando diseño de TecnicoItem

--- app/src/main/java/com/aliens/ticketsapp/ui/components/TecnicoItems.kt
+++ app/src/main/java/com/aliens/ticketsapp/ui/components/TecnicoItems.kt
@@ -1,5 +1,7 @@
 package com.aliens.ticketsapp.ui.components
 
+import android.graphics.Paint
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -10,18 +12,23 @@ import androidx.compose.material.icons.filled.Message
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.Outline
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
 import com.aliens.ticketsapp.model.Tecnico
+import kotlin.math.round
 
 @Composable
 fun TecnicoItems(
     tecnico: Tecnico,
     navController: NavController
-){
+) {
+
     Card(
         modifier = Modifier
             .padding(8.dp),
@@ -32,8 +39,24 @@ fun TecnicoItems(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(8.dp),
-            horizontalArrangement = Arrangement.SpaceBetween
         ) {
+
+            Card(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .height(70.dp)
+                    .width(70.dp),
+                shape = RoundedCornerShape(64.dp),
+                backgroundColor = Color.Blue
+            ) {
+                Text(
+                    text = tecnico.nombreTecnico,
+                    style = MaterialTheme.typography.h4,
+                    color = Color.White,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.padding(6.dp)
+                )
+            }
 
             Column(
                 modifier = Modifier.padding(8.dp),
@@ -43,14 +66,25 @@ fun TecnicoItems(
                     style = MaterialTheme.typography.h6
                 )
                 Spacer(modifier = Modifier.height(10.dp))
-                Row(){
+                Row() {
+
                     Text(text = tecnico.email)
                 }
                 Spacer(modifier = Modifier.height(10.dp))
-                Text(text = "tel: " + tecnico.telefonoTecnico)
+                Text(
+                    text = "tel: " + tecnico.telefonoTecnico,
+                    style = MaterialTheme.typography.caption
+
+                )
             }
 
 
         }
     }
 }
+
+fun InicalNombre(inicial: String): String {
+    var i = inicial[0]
+    return i.toString()
+}
+
