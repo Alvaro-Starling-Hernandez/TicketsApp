ff06e9b74a6a33d9699017fdfa5aef7c9b9b798b
Frankelyn <Frankelyn_garcia@ucne.edu.do>
2022-07-13 10:56:47 -0400

    Colocando TecnicoItems en LazyColunm

--- app/src/main/java/com/aliens/ticketsapp/ui/screens/tecnico/ConsultaTecnicoScreen.kt
+++ app/src/main/java/com/aliens/ticketsapp/ui/screens/tecnico/ConsultaTecnicoScreen.kt
@@ -3,11 +3,13 @@ package com.aliens.ticketsapp.ui.screens.tecnico
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBackIos
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
@@ -15,19 +17,16 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.aliens.ticketsapp.R
+import com.aliens.ticketsapp.ui.components.RespuestaItem
+import com.aliens.ticketsapp.ui.components.TecnicoItems
 import com.aliens.ticketsapp.ui.navigation.NavigationSetup
 import com.aliens.ticketsapp.ui.screens.respuesta.RespuestaViewModel
 import com.aliens.ticketsapp.utils.Screen
 
 @Composable
-fun tecnicos(){
-
-}
-
-@Composable
 fun ConsultaTecnicoScreen(
     navController: NavController,
-    viewModel: RespuestaViewModel = hiltViewModel()
+    viewModel: TecnicoViewModel = hiltViewModel()
 ) {
 
     Scaffold (
@@ -67,11 +66,13 @@ fun ConsultaTecnicoScreen(
                 .absolutePadding(16.dp, 16.dp, 16.dp, 16.dp)
         ) {
 
-            //LazyColumn(modifier = Modifier.fillMaxSize()) {
-              //  items(state.coins) { Coin ->
-                //    CoinItem(coin = Coin, {})
-                //}
-            //}
+            val listaTecnicos = viewModel.tecnicos.collectAsState(initial = emptyList())
+
+            LazyColumn(modifier = Modifier.fillMaxWidth()) {
+                items(listaTecnicos.value) { tecnico ->
+                    TecnicoItems(tecnico, navController)
+                }
+            }
 
 
         }
