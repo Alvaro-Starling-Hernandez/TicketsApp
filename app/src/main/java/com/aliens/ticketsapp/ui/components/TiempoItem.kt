c8cbfeb46277e633301b913a9abe0e0b7b1f811c
Alvaro S. Hernandez Luzon <alvarostarling24@gmail.com>
2022-07-13 15:45:10 -0400

    Agregando modal la consulta de tiempos

--- app/src/main/java/com/aliens/ticketsapp/ui/components/TiempoItem.kt
+++ app/src/main/java/com/aliens/ticketsapp/ui/components/TiempoItem.kt
@@ -22,6 +22,7 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.aliens.ticketsapp.model.Respuesta
 import com.aliens.ticketsapp.model.Tiempo
 import com.aliens.ticketsapp.ui.theme.TicketsAppTheme
 import com.aliens.ticketsapp.utils.Screen
