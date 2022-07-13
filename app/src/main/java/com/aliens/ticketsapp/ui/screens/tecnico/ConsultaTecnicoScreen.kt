85192eff461d5e369aa294de8fa4d4f4e81e61de
Frankelyn <Frankelyn_garcia@ucne.edu.do>
2022-07-13 08:51:36 -0400

    Creando Consulta de tecnicos

---
+++ app/src/main/java/com/aliens/ticketsapp/ui/screens/tecnico/ConsultaTecnicoScreen.kt
@@ -0,0 +1,80 @@
+package com.aliens.ticketsapp.ui.screens.tecnico
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowBackIos
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import com.aliens.ticketsapp.R
+import com.aliens.ticketsapp.ui.navigation.NavigationSetup
+import com.aliens.ticketsapp.ui.screens.respuesta.RespuestaViewModel
+import com.aliens.ticketsapp.utils.Screen
+
+@Composable
+fun tecnicos(){
+
+}
+
+@Composable
+fun ConsultaTecnicoScreen(
+    navController: NavController,
+    viewModel: RespuestaViewModel = hiltViewModel()
+) {
+
+    Scaffold (
+        topBar = {
+
+            TopAppBar(
+                navigationIcon = {
+                    Icon(
+                        imageVector = Icons.Default.ArrowBackIos,
+                        contentDescription = stringResource(R.string.ArrowBack),
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .clickable {
+                                //navegar
+                            }
+                    )
+                },
+                title = {
+                    Text(stringResource(R.string.Tecnico))
+                },
+                actions = {
+                    IconButton(onClick = { navController.navigate(Screen.RegistroTecnico.route) }) {
+                        Icon(
+                            modifier = Modifier.size(40.dp),
+                            imageVector = Icons.Default.Add,
+                            contentDescription = stringResource(R.string.Agregar)
+                        )
+                    }
+                }
+            )
+
+        }
+            ){
+        Column(
+            modifier = Modifier
+                .padding(it)
+                .absolutePadding(16.dp, 16.dp, 16.dp, 16.dp)
+        ) {
+
+            //LazyColumn(modifier = Modifier.fillMaxSize()) {
+              //  items(state.coins) { Coin ->
+                //    CoinItem(coin = Coin, {})
+                //}
+            //}
+
+
+        }
+    }
+
+}
