09d3ae8204efed170ca271cecacc3c20cb8136be
Frankelyn <Frankelyn_garcia@ucne.edu.do>
2022-07-13 10:23:25 -0400

    Diseñando tecnicoItem

--- app/src/main/java/com/aliens/ticketsapp/ui/components/TecnicoItems.kt
+++ app/src/main/java/com/aliens/ticketsapp/ui/components/TecnicoItems.kt
@@ -1,7 +1,16 @@
 package com.aliens.ticketsapp.ui.components
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
 import com.aliens.ticketsapp.model.Tecnico
 
 @Composable
@@ -9,5 +18,31 @@ fun TecnicoItems(
     tecnico: Tecnico,
     navController: NavController
 ){
+    Card(
+        modifier = Modifier
+            .padding(8.dp),
+        elevation = 8.dp,
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
 
+            Column(
+                modifier = Modifier.padding(8.dp),
+                horizontalAlignment = Alignment.End
+            ) {
+                Text(text = tecnico.nombreTecnico)
+                Spacer(modifier = Modifier.height(10.dp))
+                Text(text = tecnico.email)
+                Spacer(modifier = Modifier.height(10.dp))
+                Text(text = tecnico.telefonoTecnico)
+            }
+
+
+        }
+    }
 }
