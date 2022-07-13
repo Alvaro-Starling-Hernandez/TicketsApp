0e721bd095270804f7428175f912fb5290007542
Frankelyn <Frankelyn_garcia@ucne.edu.do>
2022-07-13 09:03:44 -0400

    Creando TecnicoItems

---
+++ app/src/main/java/com/aliens/ticketsapp/ui/components/TecnicoItems.kt
@@ -0,0 +1,13 @@
+package com.aliens.ticketsapp.ui.components
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import com.aliens.ticketsapp.model.Tecnico
+
+@Composable
+fun TecnicoItems(
+    tecnico: Tecnico,
+    navController: NavController
+){
+
+}
