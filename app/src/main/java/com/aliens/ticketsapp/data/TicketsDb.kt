6b5132e1deccded4d3ca1cd657d2de313c212427
Alvaro S. Hernandez Luzon <alvarostarling24@gmail.com>
2022-07-18 21:55:22 -0400

    Agregando entidad clientes a la base de datos

--- app/src/main/java/com/aliens/ticketsapp/data/TicketsDb.kt
+++ app/src/main/java/com/aliens/ticketsapp/data/TicketsDb.kt
@@ -2,22 +2,28 @@ package com.aliens.ticketsapp.data
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.aliens.ticketsapp.data.daos.ClienteDao
 import com.aliens.ticketsapp.data.daos.RespuestaDao
 import com.aliens.ticketsapp.data.daos.TecnicoDao
 import com.aliens.ticketsapp.data.daos.TiempoDao
+import com.aliens.ticketsapp.model.Cliente
 import com.aliens.ticketsapp.model.Respuesta
 import com.aliens.ticketsapp.model.Tecnico
 import com.aliens.ticketsapp.model.Tiempo
 
 @Database(
-    entities = [Tiempo::class,
+    entities = [
+        Tiempo::class,
         Respuesta::class,
-        Tecnico::class],
+        Tecnico::class,
+        Cliente::class
+    ],
     exportSchema = false,
-    version = 3
+    version = 4
 )
 abstract class TicketsDb : RoomDatabase() {
     abstract val tiempoDao: TiempoDao
     abstract val respuestaDao: RespuestaDao
-    abstract  val tecnicoDao: TecnicoDao
+    abstract val tecnicoDao: TecnicoDao
+    abstract val clienteDao: ClienteDao
 }
