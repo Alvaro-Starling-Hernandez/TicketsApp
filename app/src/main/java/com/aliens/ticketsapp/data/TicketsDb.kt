05155b38c1ae393a0373d1f24b01e8de422ce201
Alvaro S. Hernandez Luzon <alvarostarling24@gmail.com>
2022-07-19 12:30:18 -0400

    Agregando Ticket a la base de datos

--- app/src/main/java/com/aliens/ticketsapp/data/TicketsDb.kt
+++ app/src/main/java/com/aliens/ticketsapp/data/TicketsDb.kt
@@ -2,28 +2,24 @@ package com.aliens.ticketsapp.data
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.aliens.ticketsapp.data.daos.ClienteDao
-import com.aliens.ticketsapp.data.daos.RespuestaDao
-import com.aliens.ticketsapp.data.daos.TecnicoDao
-import com.aliens.ticketsapp.data.daos.TiempoDao
-import com.aliens.ticketsapp.model.Cliente
-import com.aliens.ticketsapp.model.Respuesta
-import com.aliens.ticketsapp.model.Tecnico
-import com.aliens.ticketsapp.model.Tiempo
+import com.aliens.ticketsapp.data.daos.*
+import com.aliens.ticketsapp.model.*
 
 @Database(
     entities = [
         Tiempo::class,
         Respuesta::class,
         Tecnico::class,
-        Cliente::class
+        Cliente::class,
+        Ticket::class
     ],
     exportSchema = false,
-    version = 4
+    version = 5
 )
 abstract class TicketsDb : RoomDatabase() {
     abstract val tiempoDao: TiempoDao
     abstract val respuestaDao: RespuestaDao
     abstract val tecnicoDao: TecnicoDao
     abstract val clienteDao: ClienteDao
+    abstract val ticketDao: TicketDao
 }
