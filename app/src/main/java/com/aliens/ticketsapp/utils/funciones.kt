7b7124d0d71329944b278707f5ff3a168930e12a
PrandiFabiel <fabiel098@gmail.com>
2022-07-20 18:58:20 -0400

    Creando archivo funciones para colocar las funciones especificas.

---
+++ app/src/main/java/com/aliens/ticketsapp/utils/funciones.kt
@@ -0,0 +1,70 @@
+package com.aliens.ticketsapp.utils
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.aliens.ticketsapp.ui.components.prioridad.PrioridadViewModel
+import com.aliens.ticketsapp.ui.screens.cliente.ClienteViewModel
+import com.aliens.ticketsapp.ui.screens.tecnico.TecnicoViewModel
+
+fun getEstado(id: Int): String {
+    var str: String = ""
+
+    if (id == 0){
+        str = "Pendiente"
+    }else if(id == 1){
+        str = "En Proceso"
+    }else if(id == 2) {
+        str = "Finalizado"
+    }
+
+    return str
+}
+
+
+@Composable
+fun getNombreCliente(cliente: Int, viewModel: ClienteViewModel = hiltViewModel()): String {
+    var name: String = ""
+    val clientes = viewModel.clientes.collectAsState(initial = emptyList())
+
+    clientes.value.forEach { element ->
+        if (cliente == element.clienteId) {
+            name = element.nombreCliente
+        }
+    }
+
+    return name
+}
+
+@Composable
+fun getNombreTecnico(tecn: Int, viewModel: TecnicoViewModel = hiltViewModel()): String {
+    var name: String = "";
+    val tecnicos = viewModel.tecnicos.collectAsState(initial = emptyList())
+
+    tecnicos.value.forEach { element ->
+        if (tecn == element.tecnicoId) {
+            name = element.nombreTecnico
+        }
+    }
+
+    return name
+}
+
+fun InicalNombre(inicial: String): String {
+    var i = inicial[0]
+    return i.toString()
+}
+
+@Composable
+fun getNombrePrioridad(prioridad: Int, viewModel: PrioridadViewModel = hiltViewModel()): String {
+    var name: String = ""
+    val prioridades = viewModel.prioridades.collectAsState(initial = emptyList())
+
+    prioridades.value.forEach { element ->
+        if (prioridad == element.prioridadId) {
+            name = element.descripcion
+        }
+    }
+
+    return name
+}
