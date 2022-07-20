fc60d0e6650ecbdb8151888d7c259e03d6cec809
PrandiFabiel <fabiel098@gmail.com>
2022-07-20 10:19:45 -0400

    Creando item de ticket.

---
+++ app/src/main/java/com/aliens/ticketsapp/ui/components/TicketItem.kt
@@ -0,0 +1,88 @@
+package com.aliens.ticketsapp.ui.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.aliens.ticketsapp.model.Ticket
+
+@Composable
+fun TicketItems(
+    ticket: Ticket,
+    navController: NavController
+) {
+    var showDialog by rememberSaveable { mutableStateOf(false) }
+
+    //ModalTecnico(showDialog, { showDialog = false }, navController, tecnico.tecnicoId, tecnico)
+
+    Card(
+        modifier = Modifier
+            .padding(8.dp)
+            .clickable { showDialog = true },
+        elevation = 8.dp,
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+        ) {
+
+            Card(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .height(70.dp)
+                    .width(70.dp),
+                shape = RoundedCornerShape(64.dp),
+                backgroundColor = Color.Blue
+            ) {
+                Text(
+                    text = InicalNombre(getNombreCliente(ticket.clienteId)),
+                    style = MaterialTheme.typography.h4,
+                    color = Color.White,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.padding(6.dp)
+                )
+            }
+
+            Column(
+                modifier = Modifier.padding(8.dp),
+            ) {
+                Text(
+                    text = getNombreCliente(ticket.clienteId),
+                    style = MaterialTheme.typography.h6
+                )
+                Spacer(modifier = Modifier.height(10.dp))
+                Row() {
+                    Spacer(modifier = Modifier.padding(4.dp))
+                    Text(
+                        text = ticket.asunto,
+                        style = MaterialTheme.typography.caption
+                    )
+                }
+                Spacer(modifier = Modifier.height(10.dp))
+                Row() {
+                    Spacer(modifier = Modifier.padding(4.dp))
+                    Text(
+                        text = ticket.estadoId.toString(),
+                        style = MaterialTheme.typography.caption
+
+                    )
+                }
+            }
+
+
+        }
+    }
+}
+
