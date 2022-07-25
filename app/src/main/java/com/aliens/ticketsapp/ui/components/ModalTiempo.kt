b6b71a8cc9314402386f2383f8818ee195e44bd8
Alvaro S. Hernandez Luzon <alvarostarling24@gmail.com>
2022-07-25 18:04:15 -0400

    Agregando que los text field sean de una unica linea

--- app/src/main/java/com/aliens/ticketsapp/ui/components/ModalTiempo.kt
+++ app/src/main/java/com/aliens/ticketsapp/ui/components/ModalTiempo.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
@@ -77,8 +78,16 @@ fun ModalTiempo(
 
                         Spacer(modifier = Modifier.height(20.dp))
                         Column {
-                            Text("Tecnico: "+ getNombreTecnico(tiempo.tecnicoId))
-                            Text("tiempo: "+tiempo.tiempo)
+                            Text(
+                                "Tecnico: " + getNombreTecnico(tiempo.tecnicoId),
+                                overflow = TextOverflow.Ellipsis,
+                                maxLines = 1
+                            )
+                            Text(
+                                "tiempo: " + tiempo.tiempo,
+                                overflow = TextOverflow.Ellipsis,
+                                maxLines = 1
+                            )
                         }
                         Spacer(modifier = Modifier.height(20.dp))
                         Row {
