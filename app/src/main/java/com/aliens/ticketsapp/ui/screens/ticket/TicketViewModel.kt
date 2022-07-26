d9279c7c6f1a5633662e1b74d05278bea05db40f
Alvaro S. Hernandez Luzon <alvarostarling24@gmail.com>
2022-07-26 18:02:19 -0400

    Agregando funciones de la appBar

--- app/src/main/java/com/aliens/ticketsapp/ui/screens/ticket/TicketViewModel.kt
+++ app/src/main/java/com/aliens/ticketsapp/ui/screens/ticket/TicketViewModel.kt
@@ -1,12 +1,11 @@
 package com.aliens.ticketsapp.ui.screens.ticket
 
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.aliens.ticketsapp.data.repositories.TicketRepository
 import com.aliens.ticketsapp.model.Ticket
+import com.aliens.ticketsapp.ui.components.appBar.SearchWidgetState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
@@ -51,5 +50,21 @@ class TicketViewModel @Inject constructor(
 
     fun buscar(id: Int): Flow<List<Ticket>> {
         return ticketRepository.buscar(id)
+    }
+
+    private val _searchWidgetState: MutableState<SearchWidgetState> =
+        mutableStateOf(value = SearchWidgetState.CLOSED)
+    val searchWidgetState: State<SearchWidgetState> = _searchWidgetState
+
+    private val _searchTextState: MutableState<String> =
+        mutableStateOf(value = "")
+    val searchTextState: State<String> = _searchTextState
+
+    fun updateSearchWidgetState(newValue: SearchWidgetState) {
+        _searchWidgetState.value = newValue
+    }
+
+    fun updateSearchTextState(newValue: String) {
+        _searchTextState.value = newValue
     }
 }
