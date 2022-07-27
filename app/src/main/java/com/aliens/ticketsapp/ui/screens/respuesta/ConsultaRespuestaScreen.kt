aac6302fa1ccc12790a7aae9394fdb523f1901a2
Alvaro S. Hernandez Luzon <alvarostarling24@gmail.com>
2022-07-26 20:52:39 -0400

    Quitando import no utilizado

--- app/src/main/java/com/aliens/ticketsapp/ui/screens/respuesta/ConsultaRespuestaScreen.kt
+++ app/src/main/java/com/aliens/ticketsapp/ui/screens/respuesta/ConsultaRespuestaScreen.kt
@@ -8,27 +8,17 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBackIos
-import androidx.compose.material.icons.filled.Close
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.aliens.ticketsapp.R
-import com.aliens.ticketsapp.model.Respuesta
 import com.aliens.ticketsapp.ui.components.RespuestaItem
-import com.aliens.ticketsapp.ui.components.SearchView
 import com.aliens.ticketsapp.ui.components.appBar.AppBarConBackIcon
 import com.aliens.ticketsapp.ui.components.appBar.SearchWidgetState
 import com.aliens.ticketsapp.utils.Screen
-import kotlinx.coroutines.flow.Flow
 
 @Composable
 fun ConsultaRespuestaScreen(
