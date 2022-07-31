c288ad538a037a5e75770f558ff13a30a96bf564
Frankelyn <Frankelyn_garcia@ucne.edu.do>
2022-07-31 07:23:14 -0400

    Creando splashScreenIcon

---
+++ app/src/main/java/com/aliens/ticketsapp/ui/screens/SplashScreen/SplashScreenIconApp.kt
@@ -0,0 +1,37 @@
+package com.aliens.ticketsapp.ui.screens.SplashScreen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.aliens.ticketsapp.R
+
+@Composable
+fun SplashScreenIconApp(){
+    SplashIcon()
+}
+
+@Composable
+fun SplashIcon(){
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Image(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .fillMaxSize(),
+            painter = painterResource(id = R.drawable.fondo21),
+            contentDescription = "TicketsApp"
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun splashIconPreview(){
+    SplashIcon()
+}
