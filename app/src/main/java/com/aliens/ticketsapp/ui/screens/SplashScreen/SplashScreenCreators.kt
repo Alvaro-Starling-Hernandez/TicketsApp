55eb4c29185dc10fb47b8dc2896dac4e868bea29
Frankelyn <Frankelyn_garcia@ucne.edu.do>
2022-07-30 21:33:25 -0400

    Colocando imagenes para el splashScreen

--- app/src/main/java/com/aliens/ticketsapp/ui/screens/SplashScreen/SplashScreenCreators.kt
+++ app/src/main/java/com/aliens/ticketsapp/ui/screens/SplashScreen/SplashScreenCreators.kt
@@ -2,22 +2,30 @@ package com.aliens.ticketsapp.ui.screens.SplashScreen
 
 import android.window.SplashScreen
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.aliens.ticketsapp.R
 
 @Composable
-fun SplashScreen(){
-
+fun SplashScreenCreators(){
+    Splash()
 }
 
 @Composable
 fun Splash(){
     Column(modifier = Modifier.fillMaxWidth()) {
-        //Image(imageVector = , contentDescription = )
+        Image(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .fillMaxSize(),
+            painter = painterResource(id = R.drawable.fondo22),
+            contentDescription = "Aliens Solutions SRL",
+
+        )
     }
 }
 
