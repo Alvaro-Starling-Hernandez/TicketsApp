22794b7d4ea19f2ac65bfd024bc6b429fbbdb7d3
Frankelyn <Frankelyn_garcia@ucne.edu.do>
2022-07-30 21:03:39 -0400

    Creando SplashScreen

---
+++ app/src/main/java/com/aliens/ticketsapp/ui/screens/SplashScreen/SplashScreen.kt
@@ -0,0 +1,28 @@
+package com.aliens.ticketsapp.ui.screens.SplashScreen
+
+import android.window.SplashScreen
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun SplashScreen(){
+
+}
+
+@Composable
+fun Splash(){
+    Column(modifier = Modifier.fillMaxWidth()) {
+        //Image(imageVector = , contentDescription = )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun splashPreview(){
+    Splash()
+}
