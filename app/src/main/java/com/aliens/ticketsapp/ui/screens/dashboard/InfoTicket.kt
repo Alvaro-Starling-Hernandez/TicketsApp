226d1ea670e524e5204af15f7ff78efcd25eb555
Frankelyn <Frankelyn_garcia@ucne.edu.do>
2022-08-02 10:39:14 -0400

    Modificando InfoTicket

--- app/src/main/java/com/aliens/ticketsapp/ui/screens/dashboard/InfoTicket.kt
+++ app/src/main/java/com/aliens/ticketsapp/ui/screens/dashboard/InfoTicket.kt
@@ -1,7 +1,12 @@
 package com.aliens.ticketsapp.ui.screens.dashboard
 
+import android.media.MediaDescription
+import androidx.compose.ui.graphics.painter.Painter
+
 data class InfoTicket(
     val title: String,
-    val valor: String
+    val valor: String,
+    val painter: Painter,
+    val contentDescription: String
 )
 
