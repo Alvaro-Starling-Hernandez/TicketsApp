fa5b8031297485425e4df318ddc6c2e935771aa0
Frankelyn <Frankelyn_garcia@ucne.edu.do>
2022-08-02 12:36:29 -0400

    Cambiando fondo amarillo por fondo naranja a cardPendientes

--- app/src/main/java/com/aliens/ticketsapp/ui/components/dashboardComps/CardTicketsPendientes.kt
+++ app/src/main/java/com/aliens/ticketsapp/ui/components/dashboardComps/CardTicketsPendientes.kt
@@ -1,8 +1,7 @@
 package com.aliens.ticketsapp.ui.components.dashboardComps
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
@@ -10,9 +9,12 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import com.aliens.ticketsapp.R
 
 @Composable
 fun CardTicketsPendientes(Pendientes: Int, Urgentes: Int, Alta: Int) {
@@ -21,9 +23,16 @@ fun CardTicketsPendientes(Pendientes: Int, Urgentes: Int, Alta: Int) {
         elevation = 10.dp,
         modifier = Modifier
             .padding(vertical = 10.dp)
-            .padding(15.dp),
-        backgroundColor = Color.Cyan
+            .padding(15.dp)
     ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.fondo_naranja),
+            contentDescription = "Fondo Naranja",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.width(250.dp)
+                .height(100.dp)
+        )
 
         Column(
             modifier = Modifier
@@ -35,20 +44,22 @@ fun CardTicketsPendientes(Pendientes: Int, Urgentes: Int, Alta: Int) {
                 overflow = TextOverflow.Ellipsis,
                 maxLines = 1,
                 modifier = Modifier.padding(top = 0.dp),
-                //fontWeight = FontWeight.Bold,
-                //color = Color.White
+                fontWeight = FontWeight.Bold,
+                color = Color.White
             )
             Text(
-                color = Color.Gray,
+                color = Color.White,
                 text = "Urgentes: $Urgentes",
                 overflow = TextOverflow.Ellipsis,
-                maxLines = 1
+                maxLines = 1,
+                fontWeight = FontWeight.Bold,
             )
             Text(
-                color = Color.Gray,
+                color = Color.White,
                 text = "Alta: $Alta",
                 overflow = TextOverflow.Ellipsis,
-                maxLines = 1
+                maxLines = 1,
+                fontWeight = FontWeight.Bold,
             )
         }
     }
