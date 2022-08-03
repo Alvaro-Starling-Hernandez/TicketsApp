e5c687cc16d52b73b8296a6ab3b8b2b32fe91868
Alvaro S. Hernandez Luzon <alvarostarling24@gmail.com>
2022-08-03 01:13:21 -0400

    Cambiando tema de la app

--- app/src/main/java/com/aliens/ticketsapp/ui/theme/Color.kt
+++ app/src/main/java/com/aliens/ticketsapp/ui/theme/Color.kt
@@ -1,8 +1,8 @@
 package com.aliens.ticketsapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
-
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+//Cambiar el nombre por blue
+val Purple200 = Color(0xFF0291d8)
+val Purple500 = Color(0xFF0275d8)
+val Purple700 = Color(0xFF0250d8)
+val Teal200 = Color(0xFFFFA500)
